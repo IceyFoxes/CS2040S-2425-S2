150322a5bdd94faf8671cffa0d5d6b47197d93e4
Iceyfoxes <e14061431@u.nus.edu>
2025-04-05 13:21:03 +0800

    Ps7 Part 3

--- ps7_part3/src/MazeSolver.java
+++ ps7_part3/src/MazeSolver.java
@@ -198,26 +198,198 @@ public class MazeSolver implements IMazeSolver {
 
 	@Override
 	public Integer bonusSearch(int startRow, int startCol, int endRow, int endCol) throws Exception {
-		// TODO: Find minimum fear level given new rules.
+		if (maze == null) {
+			throw new Exception("Oh no! You cannot call me without initializing the maze!");
+		}
+		if (startRow < 0 || startCol < 0 || startRow >= maze.getRows() || startCol >= maze.getColumns() ||
+				endRow < 0 || endCol < 0 || endRow >= maze.getRows() || endCol >= maze.getColumns()) {
+			throw new IllegalArgumentException("Invalid start/end coordinate");
+		}
+
+		if (startRow == endRow && startCol == endCol) {
+			return 0;
+		}
+
+		// Clear previous search
+		for (int i = 0; i < maze.getRows(); i++) {
+			for (int j = 0; j < maze.getColumns(); j++) {
+				fearToReach[i][j] = TRUE_WALL;
+				visited[i][j] = false;
+			}
+		}
+		estimateHeap.clear();
+
+		ComparableRoom start = new ComparableRoom(startRow, startCol, 0);
+		fearToReach[startRow][startCol] = 0;
+		estimateHeap.add(start);
+
+		return bonusSolve(endRow, endCol);
+	}
+
+	private Integer bonusSolve(int endRow, int endCol) {
+		// Dijkstra's algorithm
+		while (!estimateHeap.isEmpty()) {
+			ComparableRoom curr = estimateHeap.poll();
+			int row = curr.row;
+			int col = curr.col;
+			if (visited[row][col]) {
+				continue;
+			}
+			visited[row][col] = true;
+
+			if (row == endRow && col == endCol) {
+				return fearToReach[row][col];
+			}
+
+			for (Edge e : graph.getNeighbors(curr)) {
+				ComparableRoom neighbour = e.to;
+				int newRow = neighbour.row;
+				int newCol = neighbour.col;
+				if (visited[newRow][newCol]) {
+					continue;
+				}
+
+				// Change of relaxation here
+				int newFear = fearToReach[row][col];
+				if (e.fear == 1) {
+					newFear += 1;
+				} else if (e.fear > fearToReach[row][col]) {
+					newFear = e.fear;
+				}
+
+				if (newFear < fearToReach[newRow][newCol]) {
+					ComparableRoom newNode = new ComparableRoom(newRow, newCol, newFear);
+					fearToReach[newRow][newCol] = newFear;
+					this.estimateHeap.add(newNode);
+				}
+			}
+		}
 		return null;
 	}
 
 	@Override
 	public Integer bonusSearch(int startRow, int startCol, int endRow, int endCol, int sRow, int sCol)
 			throws Exception {
-		// TODO: Find minimum fear level given new rules and special room.
-		return null;
+		if (maze == null) {
+			throw new Exception("Oh no! You cannot call me without initializing the maze!");
+		}
+		if (startRow < 0 || startCol < 0 || startRow >= maze.getRows() || startCol >= maze.getColumns() ||
+				endRow < 0 || endCol < 0 || endRow >= maze.getRows() || endCol >= maze.getColumns()) {
+			throw new IllegalArgumentException("Invalid start/end coordinate");
+		}
+
+		if (startRow == endRow && startCol == endCol) {
+			return 0;
+		}
+
+		// Clear previous search
+		for (int i = 0; i < maze.getRows(); i++) {
+			for (int j = 0; j < maze.getColumns(); j++) {
+				fearToReach[i][j] = TRUE_WALL;
+				visited[i][j] = false;
+			}
+		}
+		estimateHeap.clear();
+
+		ComparableRoom start = new ComparableRoom(startRow, startCol, 0);
+		fearToReach[startRow][startCol] = 0;
+		estimateHeap.add(start);
+
+		return alternative(startRow, startCol, endRow, endCol, sRow, sCol);
+		// return bellmanFord(endRow, endCol, sRow, sCol);
+	}
+
+	private Integer alternative(int startRow, int startCol, int endRow, int endCol, int sRow, int sCol) {
+		/*
+		 * Case 1 can reach special and end:
+		 * start -> (end) -> special -> end
+		 * start -> end (shorter)
+		 * Case 2 cannot reach special
+		 * start -> end
+		 * Case 3 cannot reach end
+		 */
+
+		try {
+			Integer startToSpecial = bonusSearch(startRow, startCol, sRow, sCol);
+			Integer startToEnd = bonusSearch(startRow, startCol, endRow, endCol);
+			// Case 1:
+			if (startToSpecial != null && startToEnd != null) {
+				Integer path1 = startToSpecial + bonusSearch(sRow, sCol, endRow, endCol);
+				Integer path2 = startToEnd;
+				return Math.min(path1, path2);
+			}
+			// Case 2:
+			if (startToSpecial == null) {
+				return startToEnd;
+			}
+			// Case 3:
+			return startToEnd;
+		} catch (Exception e) {
+			System.out.println(e.getMessage());
+		}
+		return -1; // Shouldn't reach here
+	}
+
+	private Integer bellmanFord(int endRow, int endCol, int sRow, int sCol) {
+		// We can do Bellman Ford here since the special room is similar to a negative
+		// edge that resets our fear (always positive) to -1
+		int vertices = maze.getRows() * maze.getColumns();
+		boolean terminate = false;
+		// Traverse all edges (V - 1) times
+
+		for (int v = 1; v < vertices; v++) {
+			if (terminate) {
+				break;
+			}
+			terminate = true;
+			for (int row = 0; row < maze.getRows(); row++) {
+				for (int col = 0; col < maze.getColumns(); col++) {
+
+					// Not reachable yet
+					if (fearToReach[row][col] == Integer.MAX_VALUE) {
+						continue;
+					}
+
+					for (Edge e : graph.getNeighbors(new ComparableRoom(row, col, 0))) {
+						ComparableRoom neighbour = e.to;
+						int newRow = neighbour.row;
+						int newCol = neighbour.col;
+
+						// Change of relaxation here
+						int newFear = fearToReach[row][col];
+						if (newRow == sRow && newCol == sCol) {
+							newFear = -1;
+						} else if (e.fear == 1) {
+							newFear += 1;
+						} else if (e.fear > fearToReach[row][col]) {
+							newFear = newFear + e.fear;
+						}
+
+						if (newFear < fearToReach[newRow][newCol]) {
+							fearToReach[newRow][newCol] = newFear;
+							terminate = false;
+						}
+						System.out.println(
+								"row: " + newRow + " " + "col: " + newCol + "\n" + fearToReach[newRow][newCol]);
+					}
+				}
+			}
+		}
+		if (fearToReach[endRow][endCol] == TRUE_WALL) {
+			return null;
+		}
+		return fearToReach[endRow][endCol];
 	}
 
 	public static void main(String[] args) {
 		try {
-			Maze maze = Maze.readMaze("maze-dense.txt");
+			Maze maze = Maze.readMaze("maze-empty.txt");
 			Maze maze2 = Maze.readMaze("haunted-maze-sample.txt");
 			IMazeSolver solver = new MazeSolver();
 			solver.initialize(maze2);
 
-			System.out.println(solver.pathSearch(0, 1, 0, 5));
-			System.out.println(solver.pathSearch(1, 1, 1, 1));
+			System.out.println(solver.pathSearch(0, 0, 0, 3));
+			System.out.println(solver.bonusSearch(0, 0, 0, 3, 0, 2));
 		} catch (Exception e) {
 			e.printStackTrace();
 		}
